24d0ac07f95bdcfcb40b40c7249da99c3f0d20c4
Casey <chammond11@my.bcit.ca>
2015-10-01 17:39:17 -0700

    Added data for Roster model and images in /data folder

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('parser');
 
 /*
 | -------------------------------------------------------------------
@@ -83,7 +83,8 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
+
 
 /*
 | -------------------------------------------------------------------
@@ -126,4 +127,7 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('roster');
+
+/* End of file autoload.php */
+/* Location: ./application/config/autoload.php */
