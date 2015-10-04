cebad528176d52d3c2e8f44838e6691f8310f6de
Casey <chammond11@my.bcit.ca>
2015-10-04 00:12:26 -0700

    Added About controller and view

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -4,11 +4,11 @@
  * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
  * Our quotes model has been autoloaded, because we use it everywhere.
  * 
- * controllers/Welcome.php
+ * controllers/About.php
  *
  * ------------------------------------------------------------------------
  */
-class Welcome extends Application {
+class About extends Application {
 
     function __construct() {
         parent::__construct();
@@ -19,7 +19,7 @@ class Welcome extends Application {
     //-------------------------------------------------------------
 
     function index() {
-        $this->data['pagebody'] = 'homepage';    // this is the view we want shown
+        $this->data['pagebody'] = 'about';    // this is the view we want shown
         
         $this->render();
     }
---
+++ application/views/about.php
@@ -0,0 +1,9 @@
+<div class="row">
+    
+    <div class="col-md-8 text-center">
+        <p>This site may contain copyrighted material the use of which has not always been specifically authorized by the copyright owner. We are making such material available in an effort to advance understanding of environmental, political, human rights, economic, democracy, scientific, and social justice issues, etc. we believe this constitutes a ‘fair use’ of any such copyrighted material as provided for in section 107 of the US Copyright Law.</p>
+        <p>In accordance with Title 17 U.S.C. Section 107, the material on this site is distributed without profit to those who have expressed a prior interest in receiving the included information for research and educational purposes. For more information go to: http://www.law.cornell.edu/uscode/17/107.shtml</p>
+        <p>If you wish to use copyrighted material from this site for purposes of your own that go beyond ‘fair use’, you must obtain permission from the copyright owner.</p>
+    </div>
+    
+</div>
