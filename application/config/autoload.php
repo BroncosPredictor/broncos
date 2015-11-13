6ea09d087e2dfbde62ab58bc66ba02e9caa4c18a
Georgi Hristov <hristov798@gmail.com>
2015-11-12 17:01:08 -0800

    Configured sessions.
    Signed-off-by: Georgi Hristov <hristov798@gmail.com>

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -73,7 +73,7 @@ $autoload['libraries'] = array('parser');
 |
 |	$autoload['drivers'] = array('cache');
 */
-$autoload['drivers'] = array();
+$autoload['drivers'] = array('session');
 
 /*
 | -------------------------------------------------------------------
