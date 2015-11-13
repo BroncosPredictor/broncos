8526ae8990336024a931ffac906cd8581658cb50
Casey Hammond <casey.a.hammond@gmail.com>
2015-11-12 17:47:29 -0800

    Added roster table view, added view to select layout

---
+++ application/views/rosterTable.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <table>
+        <tr>
+            <th>Picture</th>
+            <th>Number</th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Age</th>
+        </tr>
+        {players}
+        <tr>
+            <td><img src="/data/{mug}" title="{who}"/></td>
+            <td>#{num}</td>
+            <td>{who}</td>
+            <td>{pos}</td>
+            <td>{age}</td>
+        </tr>
+        {/players}
+    </table>
+</div>
