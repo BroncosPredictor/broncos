a45a393e9ba0e6750c366ab8f289b72f666db1d1
Casey Hammond <casey.a.hammond@gmail.com>
2015-11-15 22:15:20 -0800

    Clean up style, content

--- application/views/rosterTable.php
+++ application/views/rosterTable.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row text-center">
     <form action="/roster/setSession" method="post">
         <select name="layout">
             <option value="">Select a layout</option>
@@ -11,29 +11,33 @@
             <option value="num">Jersey Number</option>
             <option value="pos">Position</option>
         </select>
-        <input type="submit" value="Go!" />
+        <input type="submit" value="Go!" id="filterGo" />
     </form>
 </div>
-<div class="row">
-    <table>
-        <tr>
-            <th>Picture</th>
-            <th>Number</th>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Age</th>
-        </tr>
-        {players}
-        <tr>
-            <td><img src="/data/{mug}" title="{who}"/></td>
-            <td>#{num}</td>
-            <td><a href="/player/{id}">{name}</a></td>
-            <td>{pos}</td>
-            <td>{age}</td>
-        </tr>
-        {/players}
+<div class="row text-center">
+    <table class="table table-condensed rosterTable">
+        <thead>
+            <tr>
+                <th>Picture</th>
+                <th>Number</th>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        <tbody>
+            {players}
+            <tr>
+                <td><img src="/data/{mug}" title="{who}"/></td>
+                <td>#{num}</td>
+                <td><a href="/player/{id}">{name}</a></td>
+                <td>{pos}</td>
+                <td>{age}</td>
+            </tr>
+            {/players}
+        </tbody>
     </table>
 </div>
-<div class="row">
+<div class="row text-center pagination">
     {pagination}
 </div>
